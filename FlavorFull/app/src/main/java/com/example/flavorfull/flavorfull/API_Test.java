f1e51731e030639f69fd6d4d71c7d71ccf8df120
JAurelio <31976335+JAurelio@users.noreply.github.com>
2017-10-08 05:47:59 -0400

    algorithm for finding recipe

--- FlavorFull/app/src/main/java/com/example/flavorfull/flavorfull/API_Test.java
+++ FlavorFull/app/src/main/java/com/example/flavorfull/flavorfull/API_Test.java
@@ -15,11 +15,9 @@ import java.net.URL;
 
 public class API_Test extends AppCompatActivity {
 
-
-
     TextView display;
     String recipeID;
-
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +25,23 @@ public class API_Test extends AppCompatActivity {
         setContentView(R.layout.activity_api__test);
 
         display = (TextView) findViewById(R.id.txt_title);
+        button = (Button) findViewById(R.id.btn_newPage);
 
         recipeID = "2c554ee192f549aab6e84b4ed75eeebb";
 
-        TestAsyncTask testAsyncTask = new TestAsyncTask(API_Test.this, "https://flavorfull.mybluemix.net/recipes?page=2", recipeID);
+
+        final TestAsyncTask testAsyncTask = new TestAsyncTask(API_Test.this, "https://flavorfull.mybluemix.net/recipes?page=2", recipeID);
         testAsyncTask.execute();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testAsyncTask.execute();
+            }
+        });
+
+
+
 
 
 

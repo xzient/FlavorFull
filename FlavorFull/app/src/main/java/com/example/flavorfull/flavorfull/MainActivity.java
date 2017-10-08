54d4d8d37409ca00ff466bbb65651ac3bd52dcd7
xzient <31088051+xzient@users.noreply.github.com>
2017-10-08 11:54:13 -0400

    new

--- FlavorFull/app/src/main/java/com/example/flavorfull/flavorfull/MainActivity.java
+++ FlavorFull/app/src/main/java/com/example/flavorfull/flavorfull/MainActivity.java
@@ -85,6 +85,11 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+
+
+        display.setText(person.display());
+
+
         spice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
